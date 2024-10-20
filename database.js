1952dcad7917b3b61b93302176e42edc984dbbdc
Ganesh <84133566+ganeshbabu258@users.noreply.github.com>
2024-10-20 12:23:56 +0530

    Update database.js

--- database.js
+++ database.js
@@ -59,5 +59,5 @@ app.post("/save-billing", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
